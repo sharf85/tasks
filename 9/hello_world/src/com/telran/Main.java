0facb6b1a5569f679b9fafa122ab7145fe9c7e0f
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2019-09-22 16:39:48 +0200

    adds first task of 9th group

---
+++ 9/hello_world/src/com/telran/Main.java
@@ -0,0 +1,17 @@
+package com.telran;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(getNumberTen());
+    }
+
+    static int getNumberTen() {
+        return 10 + 10;
+    }
+}
+
+class SecondClass {
+
+}
