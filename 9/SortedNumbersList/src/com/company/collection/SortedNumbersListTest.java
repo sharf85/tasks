e633bc9c20fd89dc6242c877701d2d304f1ac1bb
andre.reutow@gmail.com <andre.reutow@gmail.com>
2019-11-21 11:57:52 +0100

    Test comped

--- 9/SortedNumbersList/src/com/company/collection/SortedNumbersListTest.java
+++ 9/SortedNumbersList/src/com/company/collection/SortedNumbersListTest.java
@@ -10,18 +10,13 @@ public class SortedNumbersListTest {
 
 
     SortedNumbersList list;
-    SortedNumbersList listIsRemoved;
-    SortedNumbersList listRemoved;
-    SortedNumbersList listRemovedRepeat;
-    int[] array;
+    SortedNumbersList list2;
+
 
     @Before
     public void init() {
-        array = new int[]{19, 8, -6, 34, 19};
-        list = new SortedNumbersList(array);
-        listIsRemoved = new SortedNumbersList(array);
-        listRemoved = new SortedNumbersList(array);
-        listRemovedRepeat = new SortedNumbersList(array);
+        list = new SortedNumbersList(new int[]{19, 8, -6, 34, 19});
+        list2 = new SortedNumbersList(new int[]{-6, 8, 34, 0, 5, -20});
     }
 
     @Test
@@ -40,8 +35,8 @@ public class SortedNumbersListTest {
 
     @Test
     public void isRemove() {
-        for (int i = 0; i < listIsRemoved.size(); i++) {
-            assertTrue(listIsRemoved.isRemove(list.get(i)));
+        for (int i = 0; i < list.size(); i++) {
+            assertTrue(list.isRemove(list.get(i)));
         }
     }
 
@@ -58,9 +53,9 @@ public class SortedNumbersListTest {
         //array = {19, 8, -6, 34, 19};
         int[] arrayTwo = {-6, 8, 19, 19, 34};
 
-        for (int i = 0; i < listRemoved.size(); i++) {
-            listRemoved.removeById(0);
-            assertTrue(listRemoved.get(0) != arrayTwo[0]);
+        for (int i = 0; i < list.size(); i++) {
+            list.removeById(0);
+            assertTrue(list.get(0) != arrayTwo[0]);
         }
     }
 
@@ -83,21 +78,26 @@ public class SortedNumbersListTest {
 
     @Test
     public void removeRepeated() {
-        //array = {19, 8, -6, 34, 19};
-        int[] arrayTwo = {-6, 8, 19, 19, 34};
+        //sortArray =    {-6, 8, 19, 19, 34}
 
-        listRemovedRepeat.removeRepeated();
-        assertTrue(listRemovedRepeat.get(3) != 19);
+        list.removeRepeated();
+        assertTrue(list.get(3) != 19);
     }
 
+    // list  = {-6, 8, 19, 19, 34}
+    // list2 = {-6, 8, 34, 0, 5, -20}
 
     @Test
     public void intersection() {
 
+        SortedNumbersList listC = list.intersection(list2);
+        assertTrue(listC.size() == 3);
     }
 
     @Test
     public void union() {
+        SortedNumbersList listC = list.union(list2);
+        assertTrue(listC.size() == list.size() + list2.size());
     }
 }
 
