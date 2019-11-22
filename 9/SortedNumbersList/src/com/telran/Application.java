770216f62b2d92f3ba168b0909152f3407d1e80c
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2019-11-22 21:11:10 +0100

    adds all methods into SortedNumbersList

--- 9/SortedNumbersList/src/com/telran/Application.java
+++ 9/SortedNumbersList/src/com/telran/Application.java
@@ -5,19 +5,18 @@ import com.telran.collection.SortedNumbersList;
 public class Application {
 
     public static void main(String[] args) {
-        // write your code here
-        int[] array = {};
+
+//        list.display();
+//        System.out.println(list.remove(8));
+//        list.display();
+//        System.out.println(list.remove(8));
+
+//        SortedNumbersList list = new SortedNumbersList();
 
         SortedNumbersList list = new SortedNumbersList(new int[]{19, 8, -6, 34});
-        list.display();
-        System.out.println(list.remove(8));
-        list.display();
-        System.out.println(list.remove(8));
+        SortedNumbersList list2 = new SortedNumbersList(new int[]{2, -5, -6});
 
-        new SortedNumbersList().size();
-//        SortedNumbersList list2 = new SortedNumbersList(new int[]{2, -5, -6});
-
-//        SortedNumbersList list3 = list.intersection(list2);// {-6}
-//        SortedNumbersList list4 = list.union(list2);// {-6, -5, 2, 8, 19, 34}
+        SortedNumbersList list3 = list.intersection(list2);// {-6}
+        SortedNumbersList list4 = list.union(list2);// {-6, -5, 2, 8, 19, 34}
     }
 }
