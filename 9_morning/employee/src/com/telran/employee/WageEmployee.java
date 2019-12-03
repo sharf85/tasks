78ea24de577a3e7184694d0bae8cfba5ca796978
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2019-12-03 16:13:38 +0100

    adds employee project

---
+++ 9_morning/employee/src/com/telran/employee/WageEmployee.java
@@ -0,0 +1,17 @@
+package com.telran.employee;
+
+public class WageEmployee extends Employee {
+
+    public WageEmployee(int id, String name, String companyName, int basicSalary, int hoursPerMonth) {
+        super(id, name, companyName, basicSalary);
+        this.hoursPerMonth = hoursPerMonth;
+    }
+
+    private static final int MONTHLY_FULL_HOURS = 170;
+
+    int hoursPerMonth;
+
+    public int computeSalary() {
+        return basicSalary * hoursPerMonth / MONTHLY_FULL_HOURS;
+    }
+}
