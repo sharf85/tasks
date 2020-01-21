8d2a4290b9e8a3db879a1162353ff4bf81a9f17f
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-01-21 10:47:33 +0100

    fixes template_method morning

--- 9_morning/20_01_21_template_method/src/com/telran/Application.java
+++ 9_morning/20_01_21_template_method/src/com/telran/Application.java
@@ -6,7 +6,7 @@ import com.telran.action.ReverseAction;
 public class Application {
 
     public static void main(String[] args) {
-        Action reverse = new ReverseAction("file.txt");
+        Action reverse = new ReverseAction("file.txt", "output.txt");
         reverse.performAction();
     }
 }
