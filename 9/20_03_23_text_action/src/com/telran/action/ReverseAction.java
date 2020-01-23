5313f8fcec619f44d16031e08deaa7b6d3cbcc23
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-01-23 20:13:21 +0100

    adds text_action 9

---
+++ 9/20_03_23_text_action/src/com/telran/action/ReverseAction.java
@@ -0,0 +1,14 @@
+package com.telran.action;
+
+import java.util.List;
+
+public class ReverseAction extends TextAction {
+    public ReverseAction(String inputFilename, String outputFilename) {
+        super(inputFilename, outputFilename);
+    }
+
+    @Override
+    protected void performTask(List<String> content) {
+
+    }
+}
---
+++ 9/20_03_23_text_action/src/com/telran/action/SortAction.java
@@ -0,0 +1,14 @@
+package com.telran.action;
+
+import java.util.List;
+
+public class SortAction extends TextAction {
+    public SortAction(String inputFilename, String outputFilename) {
+        super(inputFilename, outputFilename);
+    }
+
+    @Override
+    protected void performTask(List<String> content) {
+
+    }
+}
