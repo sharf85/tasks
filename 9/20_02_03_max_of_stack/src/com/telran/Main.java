9bd2a34b243e9c848f1f27b30f2350f2ac802466
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-02-04 20:49:06 +0100

    completes max of stack evening

--- 9/20_02_03_max_of_stack/src/com/telran/Main.java
+++ 9/20_02_03_max_of_stack/src/com/telran/Main.java
@@ -6,13 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MaxStack<Auto> autoStack = new MaxStack<>(new BrandAutoComparator());
+        MaxStack<Auto> autoStack = new MaxStack<>();
+//        MaxStack<Auto> autoStack = new MaxStack<>(new BrandAutoComparator());
 
         Auto citroen = new Auto("Citroen", "C4", 2010);
         Auto opel = new Auto("Opel", "Astra", 2012);
+        Auto opelZafira = new Auto("Opel", "Zafira", 2012);
         Auto nissan = new Auto("Nissan", "X-Trail", 2018);
 
         autoStack.addLast(citroen);
+        autoStack.addLast(opelZafira);
         autoStack.addLast(opel);
         autoStack.addLast(nissan);
 
@@ -20,7 +23,7 @@ public class Main {
     }
 }
 
-class Auto {
+class Auto implements Comparable<Auto> {
     String brand;
     String model;
     int date;
@@ -39,6 +42,19 @@ class Auto {
                 ", date=" + date +
                 '}';
     }
+
+    @Override
+    public int compareTo(Auto other) {
+        int res = brand.compareTo(other.brand);
+        if (res != 0)
+            return res;
+
+        res = model.compareTo(other.model);
+        if (res != 0)
+            return res;
+
+        return date - other.date;
+    }
 }
 
 class BrandAutoComparator implements Comparator<Auto> {
@@ -46,5 +62,13 @@ class BrandAutoComparator implements Comparator<Auto> {
     @Override
     public int compare(Auto o1, Auto o2) {
         return o1.brand.compareTo(o2.brand);
+    }
+}
+
+class DateAutoComparator implements Comparator<Auto> {
+
+    @Override
+    public int compare(Auto o1, Auto o2) {
+        return o1.date - o2.date;
     }
 }
