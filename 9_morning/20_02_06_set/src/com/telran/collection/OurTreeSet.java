4cad83076d5dff970bada7d19cd70ebde7be94be
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-02-10 13:15:15 +0100

    changes tree set

--- 9_morning/20_02_06_set/src/com/telran/collection/OurTreeSet.java
+++ 9_morning/20_02_06_set/src/com/telran/collection/OurTreeSet.java
@@ -2,11 +2,13 @@ package com.telran.collection;
 
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.TreeSet;
 
 public class OurTreeSet<E> implements OurSet<E> {
 
     private TreeNode<E> root;
     private Comparator<E> comparator;
+    private int size;
 
     public OurTreeSet(Comparator<E> comparator) {
         this.comparator = comparator;
@@ -26,8 +28,9 @@ public class OurTreeSet<E> implements OurSet<E> {
     @Override
     public boolean add(E elt) {
         if (root == null) {
-            root = new TreeNode();
+            root = new TreeNode<>();
             root.key = elt;
+            size++;
             return true;
         }
 
@@ -45,27 +48,56 @@ public class OurTreeSet<E> implements OurSet<E> {
         current = new TreeNode<>();
         current.key = elt;
         current.parent = parent;
-        
+
         if (comparator.compare(elt, parent.key) < 0)
             parent.left = current;
         else
             parent.right = current;
+        size++;
         return true;
     }
 
     @Override
     public boolean remove(E elt) {
-        return false;
+
+        TreeNode<E> nodeToRemove = getNode(elt);
+
+        if (nodeToRemove == null)
+            return false;
+
+        if (nodeToRemove.left == null || nodeToRemove.right == null)
+            removeByFirstCase(nodeToRemove);
+        else
+            removeBySecondCase(nodeToRemove);
+
+        return true;
+    }
+
+    private void removeBySecondCase(TreeNode<E> nodeToRemove) {
+
+    }
+
+    private void removeByFirstCase(TreeNode<E> nodeToRemove) {
+
     }
 
     @Override
     public boolean contains(E elt) {
-        return false;
+        return (getNode(elt) != null);
+    }
+
+    private TreeNode<E> getNode(E elt) {
+        TreeNode<E> current = root;
+
+        while (current != null && comparator.compare(current.key, elt) != 0) {
+            current = comparator.compare(elt, current.key) < 0 ? current.left : current.right;
+        }
+        return current;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
