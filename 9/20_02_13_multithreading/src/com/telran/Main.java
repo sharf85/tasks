9b8150af625e1e4b1e7a138b24a99566e8b99ffb
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-02-13 21:02:35 +0100

    completes multithreading evening

--- 9/20_02_13_multithreading/src/com/telran/Main.java
+++ 9/20_02_13_multithreading/src/com/telran/Main.java
@@ -4,12 +4,24 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        SymbolPrinter printer = new SymbolPrinter(100, '*');
-        Thread th = new Thread(printer);
+        SymbolPrinter printer1 = new SymbolPrinter(100, '*');
+        Thread th1 = new Thread(printer1);
+        SymbolPrinter printer2 = new SymbolPrinter(100, '=');
+        Thread th2 = new Thread(printer2);
+        SymbolPrinter printer3 = new SymbolPrinter(100, '+');
+        Thread th3 = new Thread(printer3);
+
+        th1.setDaemon(true);
+        th2.setDaemon(true);
+        th3.setDaemon(true);
 
         long currentTime = System.currentTimeMillis();
-        th.start();
-        th.join();
+        th1.start();
+        th2.start();
+        th3.start();
+//        th1.join();
+//        th2.join();
+//        th3.join();
 
         long executionTime = System.currentTimeMillis() - currentTime;
         System.out.println("Execution time " + executionTime);
