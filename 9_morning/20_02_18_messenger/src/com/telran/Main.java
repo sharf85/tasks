d10fbec0eb60279946998657f5b73efa46fbb1dc
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-02-20 12:28:00 +0100

    completes messenger morning

--- 9_morning/20_02_18_messenger/src/com/telran/Main.java
+++ 9_morning/20_02_18_messenger/src/com/telran/Main.java
@@ -1,15 +1,20 @@
 package com.telran;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         Messenger messenger = new Messenger();
+        PrintWriter pw = new PrintWriter("output.txt");
 
         Thread sender = new Sender(messenger);
+        sender.start();
 
         Thread[] receivers = new Thread[5];
         for (int i = 0; i < receivers.length; i++) {
-            receivers[i] = new Receiver(messenger);
+            receivers[i] = new Receiver(messenger, pw);
             receivers[i].setDaemon(true);
         }
 
@@ -17,6 +22,7 @@ public class Main {
             receivers[i].start();
         }
 
-        sender.start();
+        sender.join();
+        pw.close();
     }
 }
