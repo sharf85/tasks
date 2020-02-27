569fe22a08776530b9236dfd198f3fd1118a9dcd
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-02-27 07:11:20 +0100

    adds text and dictionary interview

---
+++ interview/20_02_26_text_and_dictionary/src/com/telran/Dictionary.java
@@ -0,0 +1,42 @@
+package com.telran;
+
+import java.util.*;
+
+public class Dictionary {
+    private Set<String> words;
+
+    public Dictionary(Set<String> words) {
+        this.words = words;
+    }
+
+    public Dictionary(Collection<String> words) {
+        this.words = new HashSet<>(words);
+    }
+
+
+    /**
+     * @param text to be split
+     * @return a list of words which the text is split on,
+     * otherwise null if the text cannot be split
+     */
+    public Deque<String> split(String text) {
+        Deque<String> result = new ArrayDeque<>();
+        return splitRecursive(text, 0, result) ? result : null;
+    }
+
+    private boolean splitRecursive(String text, int i, Deque<String> result) {
+        if (i == text.length())
+            return true;
+
+        for (String word : words) {
+            if (text.startsWith(word, i)) {
+                result.addLast(word);
+                boolean isSplit = splitRecursive(text, i + word.length(), result);
+                if (isSplit)
+                    return true;
+                result.removeLast();
+            }
+        }
+        return false;
+    }
+}
