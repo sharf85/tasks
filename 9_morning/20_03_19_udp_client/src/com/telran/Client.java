d44ad485a03b94dea92749c2bc7c18eacab8540a
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-03-20 12:53:15 +0100

    udp client, server morning

--- 9_morning/20_03_19_udp_client/src/com/telran/Client.java
+++ 9_morning/20_03_19_udp_client/src/com/telran/Client.java
@@ -6,13 +6,39 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class Client {
 
     private static final int INCOMING_DATAGRAM_SIZE = 1024;
-    private static final int PORT = 3000;
+    private static final int SERVER_PORT = 3000;
+    private static final String SERVER_HOST = "localhost";
+//    private static final String SERVER_HOST = "127.0.0.1";
 
     public static void main(String[] args) throws IOException {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress serverIp = InetAddress.getByName(SERVER_HOST);
+
+        byte[] dataIn = new byte[INCOMING_DATAGRAM_SIZE];
+        DatagramPacket packetIn = new DatagramPacket(dataIn, INCOMING_DATAGRAM_SIZE);
+
+        String line;
+        while ((line = scanner.nextLine()) != null && !line.equals("exit")) {
+            byte[] dataOut = line.getBytes();
+            DatagramPacket packetOut = new DatagramPacket(
+                    dataOut,
+                    dataOut.length,
+                    serverIp,
+                    SERVER_PORT);
+            socket.send(packetOut);
+
+            socket.receive(packetIn);
+            String res = new String(dataIn, 0, packetIn.getLength());
+            System.out.println(res);
+        }
+        socket.close();
+
     }
 }
--- 9_morning/20_03_19_udp_server/src/com/telran/Server.java
+++ 9_morning/20_03_19_udp_server/src/com/telran/Server.java
@@ -1,37 +1,94 @@
+//package com.telran;
+//
+//import java.io.IOException;
+//import java.net.DatagramPacket;
+//import java.net.DatagramSocket;
+//import java.net.InetAddress;
+//
+//public class Server {
+//
+//    static final int INCOMING_DATAGRAM_SIZE = 1024;
+//    static final int PORT = 3000;
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        DatagramSocket socket = new DatagramSocket(PORT);
+//
+//        while (true) {
+//            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];
+//            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);
+//            socket.receive(incomingPacket);
+//
+//            String line = new String(incomingData, 0, incomingPacket.getLength());
+//            line = "Hello from UDP server: " + line;
+//
+//
+//            System.out.println(incomingPacket.getPort());
+//            DatagramPacket outgoingPacket = new DatagramPacket(
+//                    line.getBytes(),
+//                    line.length(),
+//                    incomingPacket.getAddress(),
+//                    incomingPacket.getPort());
+//
+//            socket.send(outgoingPacket);
+//        }
+//    }
+//}
+
 package com.telran;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
+    private static final int INCOMING_DATAGRAM_SIZE = 1024;
+    private static final int PORT = 3000;
 
-    static final int INCOMING_DATAGRAM_SIZE = 1024;
-    static final int PORT = 3000;
 
     public static void main(String[] args) throws IOException {
-
+        // write your code here
         DatagramSocket socket = new DatagramSocket(PORT);
-        InetAddress ip = InetAddress.getByName("localhost");
-
+        ExecutorService executor = Executors.newFixedThreadPool(10);
         while (true) {
+            //received a datagram
             byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];
             DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);
             socket.receive(incomingPacket);
 
-            String line = new String(incomingData, 0, incomingPacket.getLength());
-            line = "Hello from UDP server: " + line;
+            ServerTask task = new ServerTask(socket, incomingPacket);
+            executor.execute(task);
+        }
+    }
+}
 
+class ServerTask implements Runnable {
+    private DatagramSocket socket;
+    private DatagramPacket incomingPacket;
 
-            System.out.println(incomingPacket.getPort());
+    public ServerTask(DatagramSocket socket, DatagramPacket incomingPacket) {
+        this.socket = socket;
+        this.incomingPacket = incomingPacket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String line = new String(incomingPacket.getData(), 0, incomingPacket.getLength());
+            line = "Hello from UDP server " + line;
+
             DatagramPacket outgoingPacket = new DatagramPacket(
                     line.getBytes(),
                     line.length(),
                     incomingPacket.getAddress(),
-                    incomingPacket.getPort());
+                    incomingPacket.getPort()
+            );
 
             socket.send(outgoingPacket);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
