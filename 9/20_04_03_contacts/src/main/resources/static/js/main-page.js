b7da929c8b0913dcd815eccc361f84fc4695b07c
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-05-01 17:50:46 +0200

    adds js to contacts project

--- 9/20_04_03_contacts/src/main/resources/static/js/main-page.js
+++ 9/20_04_03_contacts/src/main/resources/static/js/main-page.js
@@ -1,5 +1,19 @@
+'use strict'
 let addButton = document.querySelector("#add-contact-button");
-addButton.addEventListener("click", function (event) {
+
+let onClickAddButton = function (event) {
+    console.log("Clicked on the button itself");
     event.preventDefault();
-    this.innerHTML = "No Add!";
+    this.classList.toggle("blue");
+    if (this.classList.contains("blue")) {
+        this.classList.remove("red");
+    } else {
+        this.classList.add("red");
+    }
+}
+
+addButton.addEventListener("click", onClickAddButton);
+addButton.parentElement.addEventListener("click", function (event) {
+    console.log("Clicked on the button container");
 });
+window.variable = "My variable"
