afdb129b608d96e9f77b711cce276b12a3828556
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-09-18 16:47:50 +0200

    updates tcp server & client

--- 11/20_09_18_tcp_server/src/com/telran/Main.java
+++ 11/20_09_18_tcp_server/src/com/telran/Main.java
@@ -1,17 +1,18 @@
 package com.telran;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     static final int SERVER_PORT = 3000;
 
     public static void main(String[] args) throws IOException {
+
+        ExecutorService pool = Executors.newFixedThreadPool(10);
 
         // the server object which reserves a certain port and then listens for new connections (sockets)
         ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
@@ -20,17 +21,26 @@ public class Main {
             // the server accepts a connection
             Socket socket = serverSocket.accept();
 
-            // the object sends data to client
-            PrintStream socketOut = new PrintStream(socket.getOutputStream());
-            // the object receives data from client
-            BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String line;
-            while ((line = socketIn.readLine()) != null) {
-                String response = String.format("The string %s was accepted from the client and handled", line);
-                socketOut.println(response);
-            }
-            socket.close();
+            Runnable serverTaskThread = new ServerTask(socket);
+            pool.execute(serverTaskThread);
+//            pool.execute(() -> {
+//                try {
+//                    // the object sends data to client
+//                    PrintStream socketOut = null;
+//                    socketOut = new PrintStream(socket.getOutputStream());
+//                    // the object receives data from client
+//                    BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//                    String line;
+//                    while ((line = socketIn.readLine()) != null) {
+//                        String response = String.format("The string %s was accepted from the client and handled", line);
+//                        socketOut.println(response);
+//                    }
+//                    socket.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
         }
     }
 }
--- 11/20_09_18_tcp_server/src/com/telran/ServerTask.java
+++ 11/20_09_18_tcp_server/src/com/telran/ServerTask.java
@@ -1,8 +1,12 @@
 package com.telran;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
-public class ServerTask extends Thread {
+public class ServerTask implements Runnable {
 
     Socket socket;
 
@@ -12,6 +16,21 @@ public class ServerTask extends Thread {
 
     @Override
     public void run() {
+        try {
+            // the object sends data to client
+            PrintStream socketOut = null;
+            socketOut = new PrintStream(socket.getOutputStream());
+            // the object receives data from client
+            BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            String line;
+            while ((line = socketIn.readLine()) != null) {
+                String response = String.format("The string %s was accepted from the client and handled", line);
+                socketOut.println(response);
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
