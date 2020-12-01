3359cc4d7e7405f23a43186ea75986c7ebd66e88
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-12-01 12:28:06 +0100

    completes loan issue 14

--- 13-14/20_11_30_loan_issue/src/de/telran/LoanIssuer.java
+++ 13-14/20_11_30_loan_issue/src/de/telran/LoanIssuer.java
@@ -12,8 +12,44 @@ public class LoanIssuer {
         this.isLazy = isLazy;
     }
 
-    //TODO implement
+    /**
+     * This issuer decides whether to provide a loan to the customer by the following rules:
+     * 1. If the customer earns less than 20000 gold or he is younger than 18 y.o. then no loan
+     * may be provided.
+     * 2. if this issuer is lazy, then matching these two conditions would be enough for him to
+     * provide a loan (meaning, the customer earns >= 20000 gold and he is 18 or older)
+     * 3. Else if this issuer is kind, they provide a load if the customer is younger than 70 y.o.
+     * 4. Else this issuer provides the loan in case if the customer is younger than 50 y.o.
+     *
+     * @param loanConsumer the customer whom to provide a loan
+     * @return whether the loan is provided
+     */
     public boolean toIssue(LoanConsumer loanConsumer) {
+//        if (loanConsumer.getAge() >= 18 && loanConsumer.getAnnualIncome() >= 20000) {
+//            if (this.isLazy)
+//                return true;
+//
+//            if (loanConsumer.getAge() < 50)
+//                return true;
+//
+//            if (this.isKind && loanConsumer.getAge() < 70)
+//                return true;
+//        }
+//
+//        return false;
+        
+        if (loanConsumer.getAge() < 18 || loanConsumer.getAnnualIncome() < 20000)
+            return false;
+
+        if (this.isLazy)
+            return true;
+
+        if (loanConsumer.getAge() < 50)
+            return true;
+
+        if (this.isKind && loanConsumer.getAge() < 70)
+            return true;
+
         return false;
     }
 
--- 13-14/20_11_30_loan_issue/src/de/telran/LoanIssuerTest.java
+++ 13-14/20_11_30_loan_issue/src/de/telran/LoanIssuerTest.java
@@ -2,10 +2,162 @@ package de.telran;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LoanIssuerTest {
 
     @Test
-    public void test() {
+    public void testToIssue_lazyAndKindIssuer_providesEverybodyOlder18AndEarningMoreOrEqualThan20000() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", true, true);
 
+        LoanConsumer customerMiddleAged = new LoanConsumer("Arkady", 35, 20000);
+        LoanConsumer customerOlderAged = new LoanConsumer("Fuka", 50, 20000);
+        LoanConsumer customerOlderAgedOlder = new LoanConsumer("Varlam", 55, 20000);
+        LoanConsumer customerSenior = new LoanConsumer("Karp", 70, 20000);
+        LoanConsumer customerUndead = new LoanConsumer("Koschey", 200, 20000);
+
+        assertTrue(issuer.toIssue(customerMiddleAged));
+        assertTrue(issuer.toIssue(customerOlderAged));
+        assertTrue(issuer.toIssue(customerOlderAgedOlder));
+        assertTrue(issuer.toIssue(customerSenior));
+        assertTrue(issuer.toIssue(customerUndead));
+    }
+
+    @Test
+    public void testToIssue_lazyAndEvilIssuer_providesEverybodyOlder18AndEarningMoreOrEqualThan20000() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", false, true);
+
+        LoanConsumer customerMiddleAged = new LoanConsumer("Arkady", 35, 20000);
+        LoanConsumer customerOlderAged = new LoanConsumer("Fuka", 50, 20000);
+        LoanConsumer customerOlderAgedOlder = new LoanConsumer("Varlam", 55, 20000);
+        LoanConsumer customerSenior = new LoanConsumer("Karp", 70, 20000);
+        LoanConsumer customerUndead = new LoanConsumer("Koschey", 200, 20000);
+
+        assertTrue(issuer.toIssue(customerMiddleAged));
+        assertTrue(issuer.toIssue(customerOlderAged));
+        assertTrue(issuer.toIssue(customerOlderAgedOlder));
+        assertTrue(issuer.toIssue(customerSenior));
+        assertTrue(issuer.toIssue(customerUndead));
+    }
+
+    @Test
+    public void testToIssue_lazyAndEvilIssuer_notProvidesEverybodyOlder18AndEarningLessThan20000() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", false, true);
+
+        LoanConsumer customerMiddleAged = new LoanConsumer("Arkady", 35, 19999);
+        LoanConsumer customerOlderAged = new LoanConsumer("Fuka", 50, 19999);
+        LoanConsumer customerOlderAgedOlder = new LoanConsumer("Varlam", 55, 19000);
+        LoanConsumer customerSenior = new LoanConsumer("Karp", 70, 19000);
+        LoanConsumer customerUndead = new LoanConsumer("Koschey", 200, 19000);
+
+        assertFalse(issuer.toIssue(customerMiddleAged));
+        assertFalse(issuer.toIssue(customerOlderAged));
+        assertFalse(issuer.toIssue(customerOlderAgedOlder));
+        assertFalse(issuer.toIssue(customerSenior));
+        assertFalse(issuer.toIssue(customerUndead));
+    }
+
+    @Test
+    public void testToIssue_lazyAndKindIssuer_notProvidesEverybodyOlder18AndEarningLessThan20000() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", true, true);
+
+        LoanConsumer customerMiddleAged = new LoanConsumer("Arkady", 35, 19999);
+        LoanConsumer customerOlderAged = new LoanConsumer("Fuka", 50, 19999);
+        LoanConsumer customerOlderAgedOlder = new LoanConsumer("Varlam", 55, 19000);
+        LoanConsumer customerSenior = new LoanConsumer("Karp", 70, 19000);
+        LoanConsumer customerUndead = new LoanConsumer("Koschey", 200, 19000);
+
+        assertFalse(issuer.toIssue(customerMiddleAged));
+        assertFalse(issuer.toIssue(customerOlderAged));
+        assertFalse(issuer.toIssue(customerOlderAgedOlder));
+        assertFalse(issuer.toIssue(customerSenior));
+        assertFalse(issuer.toIssue(customerUndead));
+    }
+
+    @Test
+    public void testToIssue_lazyAndKindIssuer_notProvidesEverybodyYounger18() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", true, true);
+
+        LoanConsumer customerYoungFewGold = new LoanConsumer("Arkady", 17, 19999);
+        LoanConsumer customerYoungNormalGold = new LoanConsumer("Arkady", 17, 21000);
+
+        assertFalse(issuer.toIssue(customerYoungFewGold));
+        assertFalse(issuer.toIssue(customerYoungNormalGold));
+    }
+
+    @Test
+    public void testToIssue_lazyAndEvilIssuer_notProvidesEverybodyYounger18() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", false, true);
+
+        LoanConsumer customerYoungFewGold = new LoanConsumer("Arkady", 17, 19999);
+        LoanConsumer customerYoungNormalGold = new LoanConsumer("Arkady", 17, 21000);
+
+        assertFalse(issuer.toIssue(customerYoungFewGold));
+        assertFalse(issuer.toIssue(customerYoungNormalGold));
+    }
+
+    @Test
+    public void testToIssue_activeAndEvilIssuer_provides() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", false, false);
+
+        LoanConsumer customerMiddleAged = new LoanConsumer("Arkady", 25, 20000);
+        LoanConsumer customerAlmostOlderAged = new LoanConsumer("Arkady Georgievich", 49, 25000);
+
+        assertTrue(issuer.toIssue(customerMiddleAged));
+        assertTrue(issuer.toIssue(customerAlmostOlderAged));
+    }
+
+    @Test
+    public void testToIssue_activeAndEvilIssuer_notProvides() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", false, false);
+
+        LoanConsumer customerMiddleAgedFewGold = new LoanConsumer("Arkady", 25, 19999);
+        LoanConsumer customerAlmostOlderAgedFewGold = new LoanConsumer("Arkady Georgievich", 49, 19999);
+        LoanConsumer customerOlderAgedFewGold = new LoanConsumer("Arkady Georgievich", 50, 19999);
+        LoanConsumer customerSeniorFewGold = new LoanConsumer("Arkady Georgievich", 70, 19999);
+
+        LoanConsumer customerYoungButReach = new LoanConsumer("Arkady Junior", 17, 25000);
+        LoanConsumer customerOlderNormalGold = new LoanConsumer("Arkady Georgievich", 50, 25000);
+
+        assertFalse(issuer.toIssue(customerMiddleAgedFewGold));
+        assertFalse(issuer.toIssue(customerAlmostOlderAgedFewGold));
+        assertFalse(issuer.toIssue(customerOlderAgedFewGold));
+        assertFalse(issuer.toIssue(customerOlderNormalGold));
+        assertFalse(issuer.toIssue(customerSeniorFewGold));
+        assertFalse(issuer.toIssue(customerYoungButReach));
+    }
+
+    @Test
+    public void testToIssue_activeAndKindIssuer_provides() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", true, false);
+
+        LoanConsumer customerMiddleAged = new LoanConsumer("Arkady", 25, 20000);
+        LoanConsumer customerAlmostOlderAged = new LoanConsumer("Arkady Georgievich", 49, 25000);
+        LoanConsumer customerOlderAged = new LoanConsumer("Arkady Georgievich", 69, 25000);
+
+        assertTrue(issuer.toIssue(customerMiddleAged));
+        assertTrue(issuer.toIssue(customerAlmostOlderAged));
+        assertTrue(issuer.toIssue(customerOlderAged));
+    }
+
+    @Test
+    public void testToIssue_activeAndKindIssuer_notProvides() {
+        LoanIssuer issuer = new LoanIssuer("Vasily", false, false);
+
+        LoanConsumer customerMiddleAgedFewGold = new LoanConsumer("Arkady", 25, 19999);
+        LoanConsumer customerAlmostOlderAgedFewGold = new LoanConsumer("Arkady Georgievich", 49, 19999);
+        LoanConsumer customerOlderAgedFewGold = new LoanConsumer("Arkady Georgievich", 50, 19999);
+        LoanConsumer customerSeniorFewGold = new LoanConsumer("Arkady Georgievich", 70, 19999);
+
+        LoanConsumer customerYoungButReach = new LoanConsumer("Arkady Junior", 17, 25000);
+        LoanConsumer customerOlderNormalGold = new LoanConsumer("Arkady Georgievich", 70, 25000);
+
+        assertFalse(issuer.toIssue(customerMiddleAgedFewGold));
+        assertFalse(issuer.toIssue(customerAlmostOlderAgedFewGold));
+        assertFalse(issuer.toIssue(customerOlderAgedFewGold));
+        assertFalse(issuer.toIssue(customerOlderNormalGold));
+        assertFalse(issuer.toIssue(customerSeniorFewGold));
+        assertFalse(issuer.toIssue(customerYoungButReach));
     }
 }
