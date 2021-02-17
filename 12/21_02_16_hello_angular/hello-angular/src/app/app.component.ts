544bf8d35a8973be47dc852e264be5145c9e7d1f
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2021-02-17 18:54:30 +0100

    changes hello angular 12

--- 12/21_02_16_hello_angular/hello-angular/src/app/app.component.ts
+++ 12/21_02_16_hello_angular/hello-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title = 'Hello Angular!';
+
+  onClickButton(): void {
+    alert('The button clicked');
+  }
 }
