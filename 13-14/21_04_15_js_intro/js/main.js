30ca934ba8d446e58cfb1238e0aba57eaacb1343
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2021-04-16 14:12:42 +0200

    modifies js intro 14

--- 13-14/21_04_15_js_intro/js/main.js
+++ 13-14/21_04_15_js_intro/js/main.js
@@ -1,1 +1,18 @@
 console.log("Hello world")
+
+function f1() {
+    console.log("inside f1");
+}
+
+const a = 10;
+let f2 = function (number) {
+    return number * 2;
+}
+let b = f2(a);
+
+console.log(`b equals to ${b}`)
+
+b = f2;
+
+console.log(b(a))
+
