77d352554fe1ee6b7c942b8aa297f82b435019bf
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2021-04-22 11:07:12 +0200

    completes dom elements 14

--- 13-14/21_04_21_dom_elements/main.js
+++ 13-14/21_04_21_dom_elements/main.js
@@ -1,9 +1,12 @@
-document.addEventListener("DOMContentLoaded", main);
-
-function main() {
+document.addEventListener("DOMContentLoaded", function () {
     let marsText = document.querySelector(".mars")
     marsText.id = "first-text"
     console.log(marsText.id)
-}
 
+// make the text red/non red while clicking on it
 
+    marsText.addEventListener("click", function (event) {
+        // marsText.classList.toggle("red");
+        event.target.classList.toggle("red");
+    });
+});
